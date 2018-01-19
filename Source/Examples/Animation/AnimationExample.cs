a3f15dfcc104e7c31b02ce701826eb3603590917
Roman Fadeev <roma.fadeev@gmail.com>
2018-01-19 23:11:49 +0800

    Support adding start and end callbacks to Animation component clips

---
+++ Source/Examples/Animation/AnimationExample.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UnityForge.AnimCallbacks.Examples
+{
+    public class AnimationExample : MonoBehaviour
+    {
+        [SerializeField]
+        private Animation exampleAnimation;
+        [SerializeField]
+        private string clipName;
+
+        private void Start()
+        {
+            exampleAnimation.OnClipStart(clipName, () => Debug.LogFormat("{0} clip started", clipName));
+            exampleAnimation.OnClipEnd(clipName, () => Debug.LogFormat("{0} clip ended", clipName));
+            exampleAnimation.Play(clipName);
+        }
+    }
+}
---
+++ Source/Extensions/AnimationExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityForge.AnimCallbacks
+{
+    public static class AnimationExtensions
+    {
+        private const string OnStartEventRaisedMethodName = "OnStartEventRaised";
+        private const string OnEndEventRaisedMethodName = "OnEndEventRaised";
+
+        public static void OnClipStart(this Animation animation, string clipName, Action callback)
+        {
+            animation.OnClipStartOrEnd(clipName, callback, true);
+        }
+
+        public static void OnClipEnd(this Animation animation, string clipName, Action callback)
+        {
+            animation.OnClipStartOrEnd(clipName, callback, false);
+        }
+
+        private static void OnClipStartOrEnd(this Animation animation, string clipName, Action callback, bool isStart)
+        {
+            if (callback == null)
+            {
+                Debug.LogWarning("Trying to register null callback for animation's clip " + (isStart ? "start" : "end"));
+                return;
+            }
+
+            var clip = animation.GetClip(clipName);
+            if (clip == null)
+            {
+                Debug.LogWarning("Failed to get animation clip");
+                return;
+            }
+
+            var eventReceiver = animation.gameObject.GetComponent<AnimationEventReceiver>();
+            if (eventReceiver == null)
+            {
+                eventReceiver = animation.gameObject.AddComponent<AnimationEventReceiver>();
+            }
+
+            if (isStart)
+            {
+                eventReceiver.RegisterAnimationStartCallback(callback);
+                clip.AddEventIfNotExists(OnStartEventRaisedMethodName, 0.0f);
+            }
+            else
+            {
+                eventReceiver.RegisterAnimationEndCallback(callback);
+                clip.AddEventIfNotExists(OnEndEventRaisedMethodName, clip.length);
+            }
+        }
+    }
+}
